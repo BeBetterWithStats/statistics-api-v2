faf3f2de0913b7022fc1465115fe8e72f0e913c8
RemyGuilloux <guillouxrem@gmail.com>
2019-05-22 12:28:44 +0200

    erreur de branche remise à l'etat initial de la branche master

--- src/test/java/fr/bbws/api/statistics/river/PlateAppearanceRiver.java
+++ src/test/java/fr/bbws/api/statistics/river/PlateAppearanceRiver.java
@@ -73,7 +73,7 @@ public class PlateAppearanceRiver {
 		ArrayList<Path> file_directories = new ArrayList<Path>();
 		// file_directories.add(Paths.get("/Users/alexandrelods/Documents/Developpement/bbws/Games/stats 2019 D2 FR"));
 		// file_directories.add(Paths.get("/Users/alexandrelods/Documents/Developpement/bbws/Games/stats 2019 D1 FR"));
-		file_directories.add(Paths.get("/Users/A477315/workspace/statistic/statistics-api-v2-master/fichier"));
+		file_directories.add(Paths.get("/Users/alexandrelods/Documents/Developpement/bbws/Games/ForTest"));
 
 		logger.info("##########  ----------  ##########");
 		logger.info("##########  BBWS RIVER  ##########");
