3b557da6e250f7ccd4dc57832f86f77fdde8d795
RemyGuilloux <guillouxrem@gmail.com>
2019-05-22 12:18:01 +0200

    ajout de la méthode PlayerList

--- src/main/java/fr/bbws/api/statistics/PlateAppearenceResource.java
+++ src/main/java/fr/bbws/api/statistics/PlateAppearenceResource.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -34,82 +36,107 @@ import fr.bbws.api.statistics.service.PlateAppearanceService;
 public class PlateAppearenceResource {
 
 	final static Logger logger = LogManager.getLogger(PlateAppearenceResource.class.getName());
-	    
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response add(Map<String, Object> p_pa) {
-    	
-    	logger.info("[{}] add the followed plate-appearance {}", "ENTRY", p_pa);
-    	
-    	try {
-    		
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response add(Map<String, Object> p_pa) {
+
+		logger.info("[{}] add the followed plate-appearance {}", "ENTRY", p_pa);
+
+		try {
+
 			Map<String, Object> result = new TreeMap<String, Object>();
 			result = new PlateAppearanceService().add(p_pa);
 			String json = new GsonBuilder().create().toJson(result);
 			return Response.status(Status.CREATED).entity(json)
 					.header("Access-Control-Allow-Origin", "*")
 					.build();
-		
-    	} catch (BadRequestException e) {
-    		return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
-		} catch (InternalErrorException e) {
-    		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-		}
-    }
-    
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response list(
-    		@QueryParam("search") String p_who, 
-    		@QueryParam("sort") String p_sort,
-    		@QueryParam("limit") int p_size) {
 
-    	logger.info("[{}] list all plate-appearance for the player {}", "ENTRY", p_who);
-    	logger.info("[{}]                 with the query parameter {} {}", "ENTRY", "sort", p_sort);
-    	logger.info("[{}]                 with the query parameter {} {}", "ENTRY", "limit", p_size);
-    	
-    	try {
-    		
+		} catch (BadRequestException e) {
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+		} catch (InternalErrorException e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
+	}
+
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response list(
+			@QueryParam("search") String p_who,
+			@QueryParam("sort") String p_sort,
+			@QueryParam("limit") int p_size) {
+
+
+		logger.info("[{}] list all plate-appearance for the player {}", "ENTRY", p_who);
+		logger.info("[{}]                 with the query parameter {} {}", "ENTRY", "sort", p_sort);
+		logger.info("[{}]                 with the query parameter {} {}", "ENTRY", "limit", p_size);
+
+		try {
+
 			List<Object> result = new ArrayList<Object>();
 			result = new PlateAppearanceService().list(p_who, p_sort, p_size);
 			String json = new GsonBuilder().create().toJson(result);
 			logger.debug("[{}] @return json = {}", "EXIT", json);
-	    	return Response.ok().entity(json)
-	    			.header("Access-Control-Allow-Origin", "*")
+			return Response.ok().entity(json)
+					.header("Access-Control-Allow-Origin", "*")
 					.build();
-		
-    	} catch (BadRequestException e) {
-    		return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+
+		} catch (BadRequestException e) {
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
 		} catch (InternalErrorException e) {
-    		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
 		}
-   }
+	}
+
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/player")
+	public Response getPlayer() throws InternalErrorException  {
 
 
-   @GET 
-   @Produces(MediaType.APPLICATION_JSON)
-   @Path("/{id}")
-   public Response get(@PathParam("id") String p_ID) {
-	   
-	   	logger.info("[{}] get the plate-appearance", "ENTRY");
-	   	logger.info("[{}]    with the @PathParam = '{}'", "ENTRY", p_ID);
-	   	
-	   	try {
-    		
-	   		Map< String, Object> result = new HashMap<>();
+		try {
+
+
+			Set<Object> result =  new TreeSet<Object>();
+			result = new PlateAppearanceService().Playerlist();
+			String json = new GsonBuilder().create().toJson(result);
+			logger.debug("[{}] @return json = {}", "EXIT", json);
+			return Response.ok().entity(json)
+					.header("Access-Control-Allow-Origin", "*")
+					.build();
+
+		} catch (BadRequestException e) {
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+		}
+	}
+
+
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Response get(@PathParam("id") String p_ID) {
+
+		logger.info("[{}] get the plate-appearance", "ENTRY");
+		logger.info("[{}]    with the @PathParam = '{}'", "ENTRY", p_ID);
+
+		try {
+
+			Map< String, Object> result = new HashMap<>();
 			result = new PlateAppearanceService().get(p_ID);
 			String json = new GsonBuilder().create().toJson(result);
 			logger.debug("[{}] @return json = {}", "EXIT", json);
-	    	return Response.ok().entity(json)
-	    			.header("Access-Control-Allow-Origin", "*")
+			return Response.ok().entity(json)
+					.header("Access-Control-Allow-Origin", "*")
 					.build();
-		
-    	} catch (BadRequestException e) {
-    		return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+
+		} catch (BadRequestException e) {
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
 		} catch (NotFoundException e) {
-    		return Response.status(Status.NOT_FOUND).entity(e.getMessage()).build();
+			return Response.status(Status.NOT_FOUND).entity(e.getMessage()).build();
 		}
-   }
+	}
 }
--- src/main/java/fr/bbws/api/statistics/service/PlateAppearanceService.java
+++ src/main/java/fr/bbws/api/statistics/service/PlateAppearanceService.java
@@ -2,9 +2,12 @@ package fr.bbws.api.statistics.service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import javax.ws.rs.core.Response.Status;
 
@@ -16,6 +19,7 @@ import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
@@ -29,76 +33,76 @@ import fr.bbws.api.statistics.mapper.ElasticSearchMapper;
 public class PlateAppearanceService {
 
 	final static Logger logger = LogManager.getLogger(PlateAppearanceService.class.getName());
-	
+
 	public PlateAppearanceService() {
 		// empty constructor
 	}
-	
-	public Map<String, Object> add(Map<String, Object> p_pa) 
+
+	public Map<String, Object> add(Map<String, Object> p_pa)
 			throws BadRequestException, InternalErrorException {
-		
+
 		logger.info("[{}] @p_pa = {}", "add", p_pa);
-		
+
 		// DEBUT -- vérification des paramètres d'entrée
 		if ( p_pa == null) {
 			throw new BadRequestException("The JSON must not be empty.");
 		}
-				
+
 		if ( !p_pa.containsKey(JSON_ATTRIBUT_STATE)
-				|| !(p_pa.get(JSON_ATTRIBUT_STATE) instanceof String) 
+				|| !(p_pa.get(JSON_ATTRIBUT_STATE) instanceof String)
 				|| StringUtils.isBlank( (String) p_pa.get(JSON_ATTRIBUT_STATE))) {
 			throw new InternalErrorException("The property '" + JSON_ATTRIBUT_STATE + "' must not be null or empty.");
 		}
-		
+
 		if ( !p_pa.containsKey(JSON_ATTRIBUT_GAME)
-				|| !(p_pa.get(JSON_ATTRIBUT_GAME) instanceof String) 
+				|| !(p_pa.get(JSON_ATTRIBUT_GAME) instanceof String)
 				|| StringUtils.isBlank( (String) p_pa.get(JSON_ATTRIBUT_GAME))) {
 			throw new BadRequestException("The property '" + JSON_ATTRIBUT_GAME + "' must not be null or empty.");
 		}
-		
+
 		if ( !p_pa.containsKey(JSON_ATTRIBUT_WHEN)
-				&& !(p_pa.get(JSON_ATTRIBUT_WHEN) instanceof String) 
+				&& !(p_pa.get(JSON_ATTRIBUT_WHEN) instanceof String)
 				&& StringUtils.isBlank( (String) p_pa.get(JSON_ATTRIBUT_WHEN))) {
 			throw new BadRequestException("The property '" + JSON_ATTRIBUT_WHEN + "' must not be null or empty.");
 		}
 
 		if ( !p_pa.containsKey(JSON_ATTRIBUT_WHO)
-				|| !(p_pa.get(JSON_ATTRIBUT_WHO) instanceof String) 
+				|| !(p_pa.get(JSON_ATTRIBUT_WHO) instanceof String)
 				|| StringUtils.isBlank( (String) p_pa.get(JSON_ATTRIBUT_WHO))) {
 			throw new BadRequestException("The property '" + JSON_ATTRIBUT_WHO + "' must not be null or empty.");
 		}
-		
+
 		if ( !p_pa.containsKey(JSON_ATTRIBUT_WHERE)
-				|| !(p_pa.get(JSON_ATTRIBUT_WHERE) instanceof String) 
+				|| !(p_pa.get(JSON_ATTRIBUT_WHERE) instanceof String)
 				|| StringUtils.isBlank( (String) p_pa.get(JSON_ATTRIBUT_WHERE))) {
 			throw new BadRequestException("The property '" + JSON_ATTRIBUT_WHERE + "' is not valid. Please read the documentation to know which values are allowed.");
 		}
 
 		if ( !p_pa.containsKey(JSON_ATTRIBUT_WHAT)
-				|| !(p_pa.get(JSON_ATTRIBUT_WHAT) instanceof String) 
+				|| !(p_pa.get(JSON_ATTRIBUT_WHAT) instanceof String)
 				|| StringUtils.isBlank( (String) p_pa.get(JSON_ATTRIBUT_WHAT))) {
 			throw new BadRequestException("The property '" + JSON_ATTRIBUT_WHAT + "' is not valid. Please read the documentation to know which values are allowed.");
 		}
 		// FIN -- vérification des paramètres d'entrée
-		
-		
+
+
 		LocalDateTime created = LocalDateTime.parse( (String) p_pa.get(JSON_ATTRIBUT_GAME));
 		created = created.withSecond( LocalDateTime.now().getSecond());
 		created = created.withNano( LocalDateTime.now().getNano());
-		
-    	Map<String, Object> result = new TreeMap<String, Object>();
-		
-    	// attributs techniques
+
+		Map<String, Object> result = new TreeMap<String, Object>();
+
+		// attributs techniques
 		result.put(ES_ATTRIBUT_CREATED, created.toString());
 		result.put(ES_ATTRIBUT_STATE, p_pa.get(JSON_ATTRIBUT_STATE));
-				
+
 		// attributs obligatoires
 		result.put(ES_ATTRIBUT_GAME, p_pa.get(JSON_ATTRIBUT_GAME));
 		result.put(ES_ATTRIBUT_WHEN, p_pa.get(JSON_ATTRIBUT_WHEN));
 		result.put(ES_ATTRIBUT_WHAT, p_pa.get(JSON_ATTRIBUT_WHAT));
 		result.put(ES_ATTRIBUT_WHERE, p_pa.get(JSON_ATTRIBUT_WHERE));
 		result.put(ES_ATTRIBUT_WHO, p_pa.get(JSON_ATTRIBUT_WHO));
-		
+
 		// attributs optionnels
 		result.put(ES_ATTRIBUT_FIELD, p_pa.get(JSON_ATTRIBUT_FIELD));
 		result.put(ES_ATTRIBUT_UMPIRE, p_pa.get(JSON_ATTRIBUT_UMPIRE));
@@ -107,164 +111,216 @@ public class PlateAppearanceService {
 		result.put(ES_ATTRIBUT_FIELD_POSITION, p_pa.get(JSON_ATTRIBUT_FIELD_POSITION));
 		result.put(ES_ATTRIBUT_BATTING_ORDER, p_pa.get(JSON_ATTRIBUT_BATTING_ORDER));
 		result.put(ES_ATTRIBUT_TEAM, p_pa.get(JSON_ATTRIBUT_TEAM));
-		
+
 		logger.debug("[{}] {}", "add", "Envoi du JSON a ElasticSearch");
 		logger.debug("     [IN] = {}", result);
-		
+
 		IndexResponse responseES = ElasticSearchMapper.getInstance().open()
 				.prepareIndex("baseball-eu", "pa").setSource(result, XContentType.JSON).get();
 
 		if (StringUtils.isNotBlank(responseES.getId())) {
-			
+
 			result.put("id", "/pa/" + responseES.getId());
 			logger.debug("     [OUT] = ID {}", Status.OK, "/pa/" + responseES.getId());
-			
+
 		} else {
-			
+
 			logger.error("     [OUT] = {}", "Response from database is null or not valid.");
 			throw new InternalErrorException("Response from database is null or not valid.");
-			
-		}
-		
-		logger.info("[{}] @return = {}", "add", result);
-    	return result;
-	}
-	
 
-	public List<Object> list(String p_who, String p_sort, int p_size) 
+		}
+
+		logger.info("[{}] @return = {}", "add", result);
+		return result;
+	}
+
+
+	public List<Object> list(String p_who, String p_sort, int p_size)
 			throws BadRequestException, InternalErrorException {
-		
+
 		logger.info("[{}] @p_who = {}", "list", p_who);
 		logger.info("[{}] @p_sort = '{}'", "list", p_sort);
 		logger.info("[{}] @p_size = '{}'", "list", p_size);
-    	
+
 		// DEBUT -- vérification des paramètres d'entrée
 		if (StringUtils.isBlank(p_who)) {
 			throw new BadRequestException("Player's name must not be null or empty.");
 		}
-		
+
 		SortOrder sort = SortOrder.DESC;
-    	if (StringUtils.isNotEmpty(p_sort)) {
-    		if ( "asc".equalsIgnoreCase(p_sort) // si &sort=asc
-    			|| " created".equalsIgnoreCase(p_sort) // ou si &sort=+created
-    			|| "created".equalsIgnoreCase(p_sort) // ou si &sort=created
-    			|| "asc(created)".equalsIgnoreCase(p_sort) // ou si &sort=asc(created)
-    			|| "created.asc".equalsIgnoreCase(p_sort)) { // ou si &sort=created.asc
-    			sort = SortOrder.ASC;
-    		} else {
-    			throw new BadRequestException("Value for the sort parameter is not valid. Please check documentation.");
-    		}
-    	} // else {sort = SortOrder.DESC;}
-    	
-    	if (p_size == 0) {
-    		p_size = ES_CONFIG_MAX_RESULT;
-    	}
-    	// FIN -- vérification des paramètres d'entrée
-		
+		if (StringUtils.isNotEmpty(p_sort)) {
+			if ( "asc".equalsIgnoreCase(p_sort) // si &sort=asc
+					|| " created".equalsIgnoreCase(p_sort) // ou si &sort=+created
+					|| "created".equalsIgnoreCase(p_sort) // ou si &sort=created
+					|| "asc(created)".equalsIgnoreCase(p_sort) // ou si &sort=asc(created)
+					|| "created.asc".equalsIgnoreCase(p_sort)) { // ou si &sort=created.asc
+				sort = SortOrder.ASC;
+			} else {
+				throw new BadRequestException("Value for the sort parameter is not valid. Please check documentation.");
+			}
+		} //else {sort = SortOrder.DESC;}
+
+		if (p_size == 0) {
+			p_size = ES_CONFIG_MAX_RESULT;
+		}
+		// FIN -- vérification des paramètres d'entrée
+
 
 		List<Object> results = new ArrayList<Object>(); // the ES search result
-		
-    	// ############## EXECUTION DE LA REQUETE
-    	// parcourir l'index _baseball-eu_
-    	// requete exacte sur l'attribut _palyer-id_
-    	// correspondant au parametre de la requete REST
+
+		// ############## EXECUTION DE LA REQUETE
+		// parcourir l'index _baseball-eu_
+		// requete exacte sur l'attribut _palyer-id_
+		// correspondant au parametre de la requete REST
 		logger.debug("    [IN] = {}", p_who);
 		SearchResponse responseES = ElasticSearchMapper.getInstance().open()
-													   				.prepareSearch(ES_CONFIG_INDEX)
-													   				.setTypes(ES_CONFIG_TYPE)
-													   		        .setSearchType(SearchType.DEFAULT)
-													   		        .setQuery(QueryBuilders.matchQuery(ES_ATTRIBUT_WHO, p_who))
-													   		        .addSort(ES_ATTRIBUT_CREATED, sort)
-													   		        .setFrom(0).setSize(p_size).setExplain(true)
-													   		        .get();
+				.prepareSearch(ES_CONFIG_INDEX)
+				.setTypes(ES_CONFIG_TYPE)
+				.setSearchType(SearchType.DEFAULT)
+				.setQuery(QueryBuilders.matchQuery(ES_ATTRIBUT_WHO, p_who))
+				.addSort(ES_ATTRIBUT_CREATED, sort)
+				.setFrom(0).setSize(p_size).setExplain(true)
+				.get();
 
-    	// ############## PARCOURIR LE RESULTAT DE LA REQUETE
-    	SearchHits hits = responseES.getHits();
-    	
-    	if ( null != responseES.getHits()) {
-    		
-	    	for (SearchHit _hit : hits) {
-	    		logger.debug("    [OUT] = {}", _hit.getSourceAsMap());
-	    		logger.debug("            /pa/{ID} = {}", "/pa/" + _hit.getId());
-	    		
-	    		Map< String, Object> _result = new TreeMap< String, Object>();
-	    		// attributs principaux only
-	    		_result.put(JSON_ATTRIBUT_ID, "/pa/" + _hit.getId());
-	    		_result.put(JSON_ATTRIBUT_GAME, _hit.getSourceAsMap().get(ES_ATTRIBUT_GAME));
-	    		_result.put(JSON_ATTRIBUT_WHEN, _hit.getSourceAsMap().get(ES_ATTRIBUT_WHEN));
-	    		_result.put(JSON_ATTRIBUT_WHAT, _hit.getSourceAsMap().get(ES_ATTRIBUT_WHAT));
-	    		_result.put(JSON_ATTRIBUT_WHERE, _hit.getSourceAsMap().get(ES_ATTRIBUT_WHERE));
-	    		_result.put(JSON_ATTRIBUT_WHO, _hit.getSourceAsMap().get(ES_ATTRIBUT_WHO));
-	    		results.add(_result);
-	    	}
-	
-	    	logger.info("[{}] @return = {}", "list", results);
-	    	return results;
-	    	
-    	} else {
-    		// en mode list le service elastic search ne renvoit jamais null
-    		// sauf en cas d'erreur interne
-    		throw new InternalErrorException("Response from database is null or not valid.");
-    	}
+		// ############## PARCOURIR LE RESULTAT DE LA REQUETE
+		SearchHits hits = responseES.getHits();
+
+		if ( null != responseES.getHits()) {
+
+			for (SearchHit _hit : hits) {
+				logger.debug("    [OUT] = {}", _hit.getSourceAsMap());
+				logger.debug("            /pa/{ID} = {}", "/pa/" + _hit.getId());
+
+				Map< String, Object> _result = new TreeMap< String, Object>();
+				// attributs principaux only
+				_result.put(JSON_ATTRIBUT_ID, "/pa/" + _hit.getId());
+				_result.put(JSON_ATTRIBUT_GAME, _hit.getSourceAsMap().get(ES_ATTRIBUT_GAME));
+				_result.put(JSON_ATTRIBUT_WHEN, _hit.getSourceAsMap().get(ES_ATTRIBUT_WHEN));
+				_result.put(JSON_ATTRIBUT_WHAT, _hit.getSourceAsMap().get(ES_ATTRIBUT_WHAT));
+				_result.put(JSON_ATTRIBUT_WHERE, _hit.getSourceAsMap().get(ES_ATTRIBUT_WHERE));
+				_result.put(JSON_ATTRIBUT_WHO, _hit.getSourceAsMap().get(ES_ATTRIBUT_WHO));
+				results.add(_result);
+			}
+
+			logger.info("[{}] @return = {}", "list", results);
+			return results;
+
+		} else {
+			// en mode list le service elastic search ne renvoit jamais null
+			// sauf en cas d'erreur interne
+			throw new InternalErrorException("Response from database is null or not valid.");
+		}
 	}
-	
-	
+
+	public Set<Object> Playerlist()
+
+			throws BadRequestException, InternalErrorException {
+
+		logger.info("[{}]", "player_list");
+
+		Set<Object> results = new TreeSet<Object>();
+		logger.debug("    [IN] = {}", "");
+		SearchResponse responseES = ElasticSearchMapper.getInstance().open()
+				.prepareSearch(ES_CONFIG_INDEX)
+				.setTypes(ES_CONFIG_TYPE)
+				.setSearchType(SearchType.QUERY_THEN_FETCH)
+				.setQuery(QueryBuilders.matchAllQuery())
+				.setFrom(0).setSize(ES_CONFIG_MAX_RESULT)
+				.get();
+
+
+
+		SearchHits hits = responseES.getHits();
+
+		if ( null != responseES.getHits()) {
+
+			for (SearchHit _hit : hits) {
+				logger.debug("    [OUT] = {}", _hit.getSourceAsString());
+				logger.debug("            /pa/{ID} = {}", "/pa/" + _hit.getId());
+
+
+
+
+				results.add( _hit.getSourceAsMap().get(ES_ATTRIBUT_WHO));
+
+			}
+
+			logger.info("[{}] @return = {}", "list", results);
+
+
+
+
+
+		} else {
+
+			throw new InternalErrorException("Response from database is null or not valid.");
+		}
+
+
+
+
+		return results;
+
+	}
+
+
+
 	public Map< String, Object> get(String p_ID)
-		throws NotFoundException, BadRequestException {
-		
+			throws NotFoundException, BadRequestException {
+
 		logger.info("[{}] @p_ID = {}", "get", p_ID);
-    	
+
 		// DEBUT -- vérification des paramètres d'entrée
 		if (StringUtils.isEmpty(p_ID)) {
 			throw new BadRequestException("ID must not be null or empty.");
 		}
 		// FIN -- vérification des paramètres d'entrée
-		
-		Map< String, Object> result = new TreeMap< String, Object>();
-		
-		// ############## EXECUTION DE LA REQUETE
-    	// parcourir l'index _baseball-eu_
-    	// correspondant au path param de la requete REST
-	   	GetResponse responseES = ElasticSearchMapper.getInstance().open()
-													   				.prepareGet(ES_CONFIG_INDEX, ES_CONFIG_TYPE, p_ID).get();
 
-	   	if (!responseES.isSourceEmpty()) {
-	   		
-		   	// attributs principaux
-    		result.put(JSON_ATTRIBUT_ID, "/pa/" + responseES.getId());
+		Map< String, Object> result = new TreeMap< String, Object>();
+
+		// ############## EXECUTION DE LA REQUETE
+		// parcourir l'index _baseball-eu_
+		// correspondant au path param de la requete REST
+		GetResponse responseES = ElasticSearchMapper.getInstance().open()
+				.prepareGet(ES_CONFIG_INDEX, ES_CONFIG_TYPE, p_ID).get();
+
+		if (!responseES.isSourceEmpty()) {
+
+			// attributs principaux
+			result.put(JSON_ATTRIBUT_ID, "/pa/" + responseES.getId());
 			result.put(JSON_ATTRIBUT_GAME, responseES.getSourceAsMap().get(ES_ATTRIBUT_GAME));
 			result.put(JSON_ATTRIBUT_WHEN, responseES.getSourceAsMap().get(ES_ATTRIBUT_WHEN));
 			result.put(JSON_ATTRIBUT_WHAT, responseES.getSourceAsMap().get(ES_ATTRIBUT_WHAT));
 			result.put(JSON_ATTRIBUT_WHERE, responseES.getSourceAsMap().get(ES_ATTRIBUT_WHERE));
 			result.put(JSON_ATTRIBUT_WHO, responseES.getSourceAsMap().get(ES_ATTRIBUT_WHO));
-			
+
 			// attributs complémentaires
 			result.put(JSON_ATTRIBUT_OPPOSITE_PITCHER, responseES.getSourceAsMap().get(ES_ATTRIBUT_OPPOSITE_PITCHER));
 			result.put(JSON_ATTRIBUT_OPPOSITE_TEAM, responseES.getSourceAsMap().get(ES_ATTRIBUT_OPPOSITE_TEAM));
 			result.put(JSON_ATTRIBUT_FIELD, responseES.getSourceAsMap().get(ES_ATTRIBUT_FIELD));
-			
-	   	} else {
 
-	   		logger.error("[{}] @return {} for the ID {}", "EXIT", Status.NOT_FOUND, p_ID);
-		   	throw new NotFoundException("The resource with the ID /pa/" + p_ID + " does not exist.");
-		   	
-	   	}
+		} else {
 
-	   	logger.info("[{}] @return {}", "get", result);
+			logger.error("[{}] @return {} for the ID {}", "EXIT", Status.NOT_FOUND, p_ID);
+			throw new NotFoundException("The resource with the ID /pa/" + p_ID + " does not exist.");
+
+		}
+
+		logger.info("[{}] @return {}", "get", result);
 		return result;
 	}
-	
+
 
 	public final static String JSON_ATTRIBUT_CREATED = "created";
 	public final static String JSON_ATTRIBUT_STATE = "state";
 	public final static String JSON_ATTRIBUT_ID = "id";
-	
+
 	public final static String JSON_ATTRIBUT_GAME = "game";
 	public final static String JSON_ATTRIBUT_WHAT = "what";
 	public final static String JSON_ATTRIBUT_WHERE = "where";
 	public final static String JSON_ATTRIBUT_WHO = "who";
 	public final static String JSON_ATTRIBUT_WHEN = "when";
-	
+
 	public final static String JSON_ATTRIBUT_FIELD = "at";
 	public final static String JSON_ATTRIBUT_UMPIRE = "umpire";
 	public final static String JSON_ATTRIBUT_OPPOSITE_PITCHER = "oppositePitcher";
@@ -272,20 +328,20 @@ public class PlateAppearanceService {
 	public final static String JSON_ATTRIBUT_FIELD_POSITION = "fieldPosition";
 	public final static String JSON_ATTRIBUT_BATTING_ORDER = "battingOrder";
 	public final static String JSON_ATTRIBUT_TEAM = "team";
-	
+
 	public final static String ES_CONFIG_INDEX = "baseball-eu";
 	public final static String ES_CONFIG_TYPE = "pa";
 	public final static int ES_CONFIG_MAX_RESULT = 1000;
 	public final static String ES_ATTRIBUT_CREATED = "created";
 	public final static String ES_ATTRIBUT_STATE = "state";
 	public final static String ES_ATTRIBUT_ID = "id";
-	
+
 	public final static String ES_ATTRIBUT_GAME = "game";
 	public final static String ES_ATTRIBUT_WHAT = "what";
 	public final static String ES_ATTRIBUT_WHERE = "where";
 	public final static String ES_ATTRIBUT_WHO = "who";
 	public final static String ES_ATTRIBUT_WHEN = "when";
-	
+
 	public final static String ES_ATTRIBUT_FIELD = "field";
 	public final static String ES_ATTRIBUT_UMPIRE = "umpire";
 	public final static String ES_ATTRIBUT_OPPOSITE_PITCHER = "opposite_pitcher";
@@ -293,5 +349,5 @@ public class PlateAppearanceService {
 	public final static String ES_ATTRIBUT_FIELD_POSITION = "field_position";
 	public final static String ES_ATTRIBUT_BATTING_ORDER = "batting_order";
 	public final static String ES_ATTRIBUT_TEAM = "team";
-	
+
 }
